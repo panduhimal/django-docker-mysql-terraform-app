e52523051b4ad17eaf015488ead605daf5a9e752
Himal Pandey <pandu.himal@gmail.com>
2024-11-01 18:11:22 +1100

    Add home page view returning a simple success message

--- dummyapp/dummyapp/urls.py
+++ dummyapp/dummyapp/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+
+def home(req):
+    return HttpResponse("<h1>Success!</h1>")
+
+
+urlpatterns = [path("admin/", admin.site.urls), path("", home, name="home")]
